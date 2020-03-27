47abcc259287cc5003826ca6e47da19d98de2729
Alain Perkaz <alain.perkaz@sennder.com>
2020-03-27 12:21:13 +0100

    improve bundle size

--- presentation/index.js
+++ presentation/index.js
@@ -24,34 +24,47 @@ import preloader from "spectacle/lib/utils/preloader";
 require("normalize.css");
 
 const images = {
-  dancingLeo: require("../assets/dancingLeo.gif"),
+  dancingLeo: require("file-loader!../assets/dancingLeo.webm"),
   gatsbyMovie: require("../assets/gatsbyMovie.jpg"),
   graphql: require("../assets/graphql.png"),
-  howGorgeous: require("../assets/howGorgeous.gif"),
+  howGorgeous: require("file-loader!../assets/howGorgeous.webm"),
   pwa: require("../assets/pwa.png"),
   react: require("../assets/react.png"),
-  thanks: require("../assets/thanks.gif"),
+  gogo: require("file-loader!../assets/gogo.webm"),
+  thanks: require("file-loader!../assets/thanks.webm"),
   webpack: require("../assets/webpack.png"),
-  whoIsGatsby: require("../assets/whoIsGatsby.gif")
+  whoIsGatsby: require("file-loader!../assets/whoIsGatsby.webm")
 };
 
 preloader(images);
 
-const theme = createTheme({
-  primary: "#000",
-  secondary: "white",
-  tertiary: "#fce715",
-  quarternary: "#3414fa"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "#000",
+    secondary: "white",
+    tertiary: "#fce715",
+    quarternary: "#3414fa"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={400} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary" bgDarken="0.7" bgImage={images.gatsbyMovie.replace("/", "")}>
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={400}
+        theme={theme}
+      >
+        <Slide
+          transition={["zoom"]}
+          bgColor="primary"
+          bgDarken="0.7"
+          bgImage={images.gatsbyMovie.replace("/", "")}
+        >
           <Heading size={6} fit caps lineHeight={1} style={{ color: "white" }}>
             The great Gatsby.js
           </Heading>
@@ -60,17 +73,26 @@ export default class Presentation extends React.Component {
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="tertiary">
-          <Image src={images.whoIsGatsby.replace("/", "")} style={{ height: "350px" }}>what gatsby</Image>
+          <video
+            autoPlay
+            loop
+            src={images.whoIsGatsby.replace("/", "")}
+            style={{ height: "350px" }}
+          >
+            what gatsby
+          </video>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="tertiary">
           <List style={{ marginTop: "0" }}>
             <ListItem>
-              <Image src={images.react.replace("/", "")}
+              <Image
+                src={images.react.replace("/", "")}
                 style={{ height: "70px" }}
               />
             </ListItem>
             <ListItem>
-              <Image src={images.webpack.replace("/", "")}
+              <Image
+                src={images.webpack.replace("/", "")}
                 style={{ height: "70px" }}
               />
             </ListItem>
@@ -78,19 +100,27 @@ export default class Presentation extends React.Component {
               <Text>JAM Stack</Text>
             </ListItem>
             <ListItem>
-              <Image src={images.graphql.replace("/", "")}
+              <Image
+                src={images.graphql.replace("/", "")}
                 style={{ height: "70px" }}
               />
             </ListItem>
             <ListItem>
-              <Image src={images.pwa.replace("/", "")}
+              <Image
+                src={images.pwa.replace("/", "")}
                 style={{ height: "50px" }}
               />
             </ListItem>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={2} textColor="quarternary" style={{ marginBottom: "20px" }} >Why</Heading>
+          <Heading
+            size={2}
+            textColor="quarternary"
+            style={{ marginBottom: "20px" }}
+          >
+            Why
+          </Heading>
           <Layout>
             <Fill>
               <Appear>
@@ -109,10 +139,23 @@ export default class Presentation extends React.Component {
           </Layout>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="tertiary">
-          <Image src={images.howGorgeous.replace("/", "")} style={{ height: "350px" }}>how gorgeous</Image>
+          <video
+            autoPlay
+            loop
+            src={images.howGorgeous.replace("/", "")}
+            style={{ height: "350px" }}
+          >
+            how gorgeous
+          </video>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={2} textColor="quarternary" style={{ marginBottom: "20px" }}>Goodies</Heading>
+          <Heading
+            size={2}
+            textColor="quarternary"
+            style={{ marginBottom: "20px" }}
+          >
+            Goodies
+          </Heading>
           <Layout>
             <Fill>
               <Appear>
@@ -131,40 +174,66 @@ export default class Presentation extends React.Component {
           </Layout>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Image src={images.dancingLeo.replace("/", "")} style={{ height: "300px" }}/>
-          <Heading size={4} textColor="quarternary" caps>Demo time</Heading>
+          <video
+            autoPlay
+            loop
+            src={images.dancingLeo.replace("/", "")}
+            style={{ height: "300px" }}
+          />
+          <Heading size={4} textColor="quarternary" caps>
+            Demo time
+          </Heading>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={4} textColor="quarternary" caps>Requisites</Heading>
-          <Text><b>node</b> : v6 and up</Text>
-          <Text><b>npm</b> : v3 and up</Text>
+          <Heading size={4} textColor="quarternary" caps>
+            Requisites
+          </Heading>
+          <Text>
+            <b>node</b> : v6 and up
+          </Text>
+          <Text>
+            <b>npm</b> : v3 and up
+          </Text>
           <Layout>
             <Fill>
               <Appear>
-                <Text textColor="pink"><b>or use Docker</b></Text>
+                <Text textColor="pink">
+                  <b>or use Docker</b>
+                </Text>
               </Appear>
             </Fill>
           </Layout>
         </Slide>
         <CodeSlide
-          bgColor="tertiary" textColor="black"
+          bgColor="tertiary"
+          textColor="black"
           transition={["fade"]}
           lang="js"
           textSize=".6em"
           code={require("raw-loader!../assets/install")}
           ranges={[
-            { loc: [0, 1], title: "Start container"},
+            { loc: [0, 1], title: "Start container" },
             { loc: [1, 2], title: "Install gatsby-cli" },
             { loc: [2, 3], title: "Get blog-starter" },
             { loc: [3, 6], title: "Start developing" }
           ]}
         />
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Image src={"https://media.giphy.com/media/eNCZy1yl5ViZa/giphy.gif"} style={{ height: "300px" }}>GoGo</Image>
-          <Heading size={4} textColor="quarternary" caps>Go-Go GraphQL</Heading>
+          <video
+            autoPlay
+            loop
+            src={images.gogo.replace("/", "")}
+            style={{ height: "300px" }}
+          >
+            GoGo
+          </video>
+          <Heading size={4} textColor="quarternary" caps>
+            Go-Go GraphQL
+          </Heading>
         </Slide>
         <CodeSlide
-          bgColor="tertiary" textColor="primary"
+          bgColor="tertiary"
+          textColor="primary"
           transition={["fade"]}
           lang="js"
           textSize=".6em"
@@ -175,24 +244,41 @@ export default class Presentation extends React.Component {
           ]}
         />
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={4} textColor="quarternary" caps>Starters</Heading>
+          <Heading size={4} textColor="quarternary" caps>
+            Starters
+          </Heading>
           <Link>https://www.gatsbyjs.org/docs/gatsby-starters</Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={4} textColor="quarternary" caps>Plugins</Heading>
+          <Heading size={4} textColor="quarternary" caps>
+            Plugins
+          </Heading>
           <Link>https://www.gatsbyjs.org/packages</Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={4} textColor="quarternary" caps>Themes</Heading>
+          <Heading size={4} textColor="quarternary" caps>
+            Themes
+          </Heading>
           <Link>http://kyleamathews.github.io/typography.js</Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
-          <Heading size={4} textColor="quarternary" caps>Ref CMS integration</Heading>
+          <Heading size={4} textColor="quarternary" caps>
+            Ref CMS integration
+          </Heading>
           <Link>https://using-contentful.netlify.com/</Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary" textColor="tertiary">
-          <Image src={images.thanks.replace("/", "")} style={{ height: "350px" }}>thanks</Image>
-          <Heading size={4} textColor="quarternary" caps>Thanks</Heading>
+          <video
+            autoPlay
+            loop
+            src={images.thanks.replace("/", "")}
+            style={{ height: "350px" }}
+          >
+            thanks
+          </video>
+          <Heading size={4} textColor="quarternary" caps>
+            Thanks
+          </Heading>
         </Slide>
       </Deck>
     );
